b691f7d981c90ee511a240ee1a5f73afe2121db4
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-07-04 11:23:28 +0200

    html nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,26 @@
+'use client'
 import React from 'react'
 import styleNav from './nav.module.css'
+import Image from 'next/image'
 export default function Nav() {
   return (
-    <div>
-        <p>ALLO</p>
-        <h1><i className="fa-solid fa-cart-shopping"></i></h1>
+    <div className={styleNav.divPNav}>
+        <Image src='/img/logo.png'width={230} height={80} alt='logo oneflix'/>
+        <p>Home</p>
+        <p>Collection</p>
+        <p>Favoris</p>
+        <div>
+            <select className={styleNav.selectNav} name="Category" defaultValue="">
+                <option value=""hidden disabled >Category</option>
+                <option value="action">Action</option>
+                <option value="adventure">Adventure</option>
+                <option value="romantic">Romantic</option>
+                <option value="comedy">Comedy</option>
+            </select>
+        </div>
+
+        <i className="fa-solid fa-cart-shopping"></i>
+        <i className="fa-solid fa-user-plus"></i>
     </div>
   )
 }
