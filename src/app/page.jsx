a5264c858a805fef0201ccdeeddbf9d8ee7e5e2b
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-07-07 12:17:35 +0200

    section 2 et 3

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -224,6 +224,137 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      <section className={styles.homeSection3}>
+        <div className={styles.homeSection1Div1}>
+          <h2>NARUTO</h2>
+          <Link href="/" className={styles.Link}>See more &rarr;</Link>
+        </div>
+        <div className={styles.divNarutoRow1}>
+
+          <div className={styles.cardWrapper}>
+          <div className={styles.card}>
+            <Image  className={styles.cardImg} src="/img/CAROU 3.jpg" width={280} height={160} alt="image de l'anime"/>
+            <div className={styles.cardOverlay}>
+              <h3 className={styles.section1CardH3}>One Piece</h3>
+              <p className={styles.section1CardP}>Prix: X</p>
+              <p className={styles.section1CardP}>Description: Luffy et sa bande à la conquête des océans</p>
+              <p className={styles.section1CardP}>Rating: 4/5</p>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.cardWrapper}>
+          <div className={styles.card}>
+            <Image  className={styles.cardImg} src="/img/CAROU 3.jpg" width={280} height={160} alt="image de l'anime"/>
+            <div className={styles.cardOverlay}>
+              <h3 className={styles.section1CardH3}>One Piece</h3>
+              <p className={styles.section1CardP}>Prix: X</p>
+              <p className={styles.section1CardP}>Description: Luffy et sa bande à la conquête des océans</p>
+              <p className={styles.section1CardP}>Rating: 4/5</p>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.cardWrapper}>
+          <div className={styles.card}>
+            <Image  className={styles.cardImg} src="/img/CAROU 3.jpg" width={280} height={160} alt="image de l'anime"/>
+            <div className={styles.cardOverlay}>
+              <h3 className={styles.section1CardH3}>One Piece</h3>
+              <p className={styles.section1CardP}>Prix: X</p>
+              <p className={styles.section1CardP}>Description: Luffy et sa bande à la conquête des océans</p>
+              <p className={styles.section1CardP}>Rating: 4/5</p>
+            </div>
+          </div>
+        </div>
+
+         
+        </div>
+        <div className={styles.divNarutoRow1}>
+
+          <div className={styles.cardWrapper}>
+          <div className={styles.card}>
+            <Image  className={styles.cardImg} src="/img/CAROU 3.jpg" width={280} height={160} alt="image de l'anime"/>
+            <div className={styles.cardOverlay}>
+              <h3 className={styles.section1CardH3}>One Piece</h3>
+              <p className={styles.section1CardP}>Prix: X</p>
+              <p className={styles.section1CardP}>Description: Luffy et sa bande à la conquête des océans</p>
+              <p className={styles.section1CardP}>Rating: 4/5</p>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.cardWrapper}>
+          <div className={styles.card}>
+            <Image  className={styles.cardImg} src="/img/CAROU 3.jpg" width={280} height={160} alt="image de l'anime"/>
+            <div className={styles.cardOverlay}>
+              <h3 className={styles.section1CardH3}>One Piece</h3>
+              <p className={styles.section1CardP}>Prix: X</p>
+              <p className={styles.section1CardP}>Description: Luffy et sa bande à la conquête des océans</p>
+              <p className={styles.section1CardP}>Rating: 4/5</p>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.cardWrapper}>
+          <div className={styles.card}>
+            <Image  className={styles.cardImg} src="/img/CAROU 3.jpg" width={280} height={160} alt="image de l'anime"/>
+            <div className={styles.cardOverlay}>
+              <h3 className={styles.section1CardH3}>One Piece</h3>
+              <p className={styles.section1CardP}>Prix: X</p>
+              <p className={styles.section1CardP}>Description: Luffy et sa bande à la conquête des océans</p>
+              <p className={styles.section1CardP}>Rating: 4/5</p>
+            </div>
+          </div>
+        </div>
+
+         
+        </div>
+
+        <div className={styles.divNarutoRow1}>
+
+          <div className={styles.cardWrapper}>
+          <div className={styles.card}>
+            <Image  className={styles.cardImg} src="/img/CAROU 3.jpg" width={280} height={160} alt="image de l'anime"/>
+            <div className={styles.cardOverlay}>
+              <h3 className={styles.section1CardH3}>One Piece</h3>
+              <p className={styles.section1CardP}>Prix: X</p>
+              <p className={styles.section1CardP}>Description: Luffy et sa bande à la conquête des océans</p>
+              <p className={styles.section1CardP}>Rating: 4/5</p>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.cardWrapper}>
+          <div className={styles.card}>
+            <Image  className={styles.cardImg} src="/img/CAROU 3.jpg" width={280} height={160} alt="image de l'anime"/>
+            <div className={styles.cardOverlay}>
+              <h3 className={styles.section1CardH3}>One Piece</h3>
+              <p className={styles.section1CardP}>Prix: X</p>
+              <p className={styles.section1CardP}>Description: Luffy et sa bande à la conquête des océans</p>
+              <p className={styles.section1CardP}>Rating: 4/5</p>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.cardWrapper}>
+          <div className={styles.card}>
+            <Image  className={styles.cardImg} src="/img/CAROU 3.jpg" width={280} height={160} alt="image de l'anime"/>
+            <div className={styles.cardOverlay}>
+              <h3 className={styles.section1CardH3}>One Piece</h3>
+              <p className={styles.section1CardP}>Prix: X</p>
+              <p className={styles.section1CardP}>Description: Luffy et sa bande à la conquête des océans</p>
+              <p className={styles.section1CardP}>Rating: 4/5</p>
+            </div>
+          </div>
+        </div>
+
+         
+        </div>
+      </section>
+    </div>
+    <div className={styles.homeDivColumn}>
+      <h1>COLUMN</h1>
     </div>
     </>
   );
